4aef2559825b742f2087041eb25e9d66ab70e0fd
Akiko Green <akikogreen08@gmail.com>
2020-04-27 02:01:38 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def find_max_value(array)
-  # Add your solution here
+  count = 0 
+  while count < array.length do
+    if array[count] == array.max 
+      return array[count]
+    end
+    count += 1
+    # binding.pry
+  end
 end
+# //////////////////// 
+# lets solve without .max method 
+
+# def find_max_value(array)
+# count = 0 
+# max_value = -1 #we need a negative varible to compare if array is all positive integers
+#   while count < array.length do
+#     if max_value < array[count]
+#       max_value = array[count]
+#     end 
+#     count += 1 
+#   end
+#   max_value  # here by the end of the loop this will be the max value returned
+# end
